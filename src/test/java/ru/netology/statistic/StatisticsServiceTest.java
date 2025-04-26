6b2a73cf9335047010a6d73bf62024195d7fb264
Noren <norenowow@gmail.com>
2025-04-27 01:12:50 +0300

    new tests added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,40 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxWhenAllValuesAreSame() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {7, 7, 7, 7, 7};
+        long expected = 7;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenArrayContainsNegativeValues() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-3, -5, -1, -7};
+        long expected = -1;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenArrayHasOneElement() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {100};
+        long expected = 100;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
